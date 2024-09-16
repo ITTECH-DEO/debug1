5b6183f93afe57539713c1a230df9f9320f28834
BachtiarAH <bachtiarah73@gmail.com>
2024-09-16 18:43:54 +0700

    dashboard done

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Repository\TicketRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DefaultController extends Controller
 {
@@ -24,14 +25,104 @@ class DefaultController extends Controller
 
         $user = $this->getUser();
 
+        $em = $this->getDoctrine()->getManager();
+        $ticketRepository = $em->getRepository('AppBundle:Ticket');
+
+        // Get filter parameters from the request
+        $selectedDepartment = $request->query->get('department', 'all');
+        $selectedPriority = $request->query->get('priority', 'all');
+        $selectedStatus = $request->query->get('status', 'all');
+        $selectedType = $request->query->get('type', 'all');
+        $selectedResponsible = $request->query->get('responsible', 'all');
+        $searchText = $request->query->get('search', '');
+
+        // Build the query with Doctrine QueryBuilder
+        $qb = $ticketRepository->createQueryBuilder('t');
+
+        if ($selectedDepartment !== 'all') {
+            $qb->andWhere('t.department = :department')
+               ->setParameter('department', $selectedDepartment);
+        }
+        if ($selectedPriority !== 'all') {
+            $qb->andWhere('t.priority = :priority')
+               ->setParameter('priority', $selectedPriority);
+        }
+        if ($selectedStatus !== 'all') {
+            $qb->andWhere('t.status = :status')
+               ->setParameter('status', $selectedStatus);
+        }
+        if ($selectedType !== 'all') {
+            $qb->andWhere('t.type = :type')
+               ->setParameter('type', $selectedType);
+        }
+        if ($selectedResponsible !== 'all') {
+            $qb->andWhere('t.assignedTo = :assigned_to')
+               ->setParameter('responsible', $selectedResponsible);
+        }
+        if (!empty($searchText)) {
+            $qb->andWhere('t.title LIKE :search OR t.content LIKE :search')
+               ->setParameter('search', '%' . $searchText . '%');
+        }
+
+        // die(dump($qb->getQuery()->getSQL()));
+
+        // Get the tickets based on the filters
+        $tickets = $qb->getQuery()->getResult();
+
+        $ticket_new = $qb->orderBy('t.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+        $tickets_ongoing = $qb->andWhere('t.status = :status')
+            ->setParameter('status', 'In Progress')
+            ->getQuery()
+            ->getResult();
+
+        $ticket_resolved = $qb->andWhere('t.status = :status')
+            ->setParameter('status', 'Validated')
+            ->getQuery()
+            ->getResult();
+
+        $ticket_my = $qb->andWhere('t.assignedTo = :assignedTo')
+            ->setParameter('assignedTo', $user->getUsername())
+            ->getQuery()
+            ->getResult();
+
+
+        // Get filter data for dropdowns
+        $departments = $ticketRepository->createQueryBuilder('t')
+            ->select('DISTINCT t.department')
+            ->getQuery()
+            ->getResult();
+
+        $priorities = $ticketRepository->createQueryBuilder('t')
+            ->select('DISTINCT t.priority')
+            ->getQuery()
+            ->getResult();
+
+        $statuses = $ticketRepository->createQueryBuilder('t')
+            ->select('DISTINCT t.status')
+            ->getQuery()
+            ->getResult();
+
+        $types = $ticketRepository->createQueryBuilder('t')
+            ->select('DISTINCT t.type')
+            ->getQuery()
+            ->getResult();
+
+        $responsibles = $ticketRepository->createQueryBuilder('t')
+            ->select('DISTINCT t.assignedTo')
+            ->where('t.assignedTo IS NOT NULL')
+            ->getQuery()
+            ->getResult();
 
         // Get the repository
-        $repository = $this->getDoctrine()->getRepository(Ticket::class);
-        $entityManager = $this->getDoctrine()->getManager();
+        // $repository = $this->getDoctrine()->getRepository(Ticket::class);
+        // $entityManager = $this->getDoctrine()->getManager();
 
 
         // Find all tickets
-        $tickets = $repository->findAll();
+        // $tickets = $repository->findAll();
 
         // Get the EntityManager
 
@@ -66,8 +157,113 @@ class DefaultController extends Controller
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
+            'tickets_admin' => $tickets_admin,
             'tickets' => $tickets,
-            'tickets_admin' => $tickets_admin
+            'ticket_new' => $ticket_new,
+            'tickets_ongoing' => $tickets_ongoing,
+            'ticket_resolved' => $ticket_resolved,
+            'ticket_my' => $ticket_my,
+            'departments' => $departments,
+            'priorities' => $priorities,
+            'statuses' => $statuses,
+            'types' => $types,
+            'responsibles' => $responsibles,
+            'selected_department' => $selectedDepartment,
+            'selected_priority' => $selectedPriority,
+            'selected_status' => $selectedStatus,
+            'selected_type' => $selectedType,
+            'selected_responsible' => $selectedResponsible,
+            'search_text' => $searchText,
+        ]);
+    }
+
+    /**
+     * @Route("/load-tab-content", name="load_tab_content")
+     */
+    public function loadTabContent(Request $request, EntityManagerInterface $em)
+    {
+        // Get the filters from the request
+        $searchText = $request->query->get('search', '');
+        $selectedDepartment = strtolower($request->query->get('department', 'all'));
+        $selectedPriority = strtolower($request->query->get('priority', 'all'));
+        $selectedStatus = strtolower($request->query->get('status', 'all'));
+        $selectedType = strtolower($request->query->get('type', 'all'));
+        $selectedResponsible = strtolower($request->query->get('responsible', 'all'));
+        $selectedWeek = strtolower($request->query->get('week', 'all'));
+
+        // Build the query
+        $queryBuilder = $em->getRepository(Ticket::class)->createQueryBuilder('t');
+        $queryBuilder->where('1=1');
+
+        if (!empty($searchText)) {
+            $queryBuilder->andWhere('t.title LIKE :search')
+                ->setParameter('search', '%' . $searchText . '%');
+        }
+
+        if ($selectedDepartment !== 'all') {
+            $queryBuilder->andWhere('LOWER(t.department) = :department')
+                ->setParameter('department', $selectedDepartment);
+        }
+
+        if ($selectedPriority !== 'all') {
+            $queryBuilder->andWhere('LOWER(t.priority) = :priority')
+                ->setParameter('priority', $selectedPriority);
+        }
+
+        if ($selectedStatus !== 'all') {
+            $queryBuilder->andWhere('LOWER(t.status) = :status')
+                ->setParameter('status', $selectedStatus);
+        }
+
+        if ($selectedType !== 'all') {
+            $queryBuilder->andWhere('LOWER(t.type) = :type')
+                ->setParameter('type', $selectedType);
+        }
+
+        if ($selectedResponsible !== 'all') {
+            $queryBuilder->andWhere('LOWER(t.assignedTo) = :responsible')
+                ->setParameter('responsible', $selectedResponsible);
+        }
+
+        if ($selectedWeek !== 'all') {
+            $startOfWeek = null;
+            $endOfWeek = null;
+
+            switch ($selectedWeek) {
+                case 'week-1':
+                    $startOfWeek = new \DateTime('first day of this month');
+                    $endOfWeek = new \DateTime('first day of this month + 6 days');
+                    break;
+                case 'week-2':
+                    $startOfWeek = new \DateTime('first day of this month + 7 days');
+                    $endOfWeek = new \DateTime('first day of this month + 13 days');
+                    break;
+                case 'week-3':
+                    $startOfWeek = new \DateTime('first day of this month + 14 days');
+                    $endOfWeek = new \DateTime('first day of this month + 20 days');
+                    break;
+                case 'week-4':
+                    $startOfWeek = new \DateTime('first day of this month + 21 days');
+                    $endOfWeek = new \DateTime('last day of this month');
+                    break;
+            }
+
+            if ($startOfWeek && $endOfWeek) {
+                $queryBuilder->andWhere('t.createdAt BETWEEN :startOfWeek AND :endOfWeek')
+                    ->setParameter('startOfWeek', $startOfWeek->format('Y-m-d'))
+                    ->setParameter('endOfWeek', $endOfWeek->format('Y-m-d'));
+            }
+        }
+
+        // Order by creation date
+        $queryBuilder->orderBy('t.createdAt', 'DESC');
+
+        // Get the result
+        $tickets = $queryBuilder->getQuery()->getResult();
+
+        // Render the view with tickets
+        return $this->render('ticket/tabContent.html.twig', [
+            'tickets' => $tickets,
         ]);
     }
 }
--- src/AppBundle/Controller/TicketController.php
+++ src/AppBundle/Controller/TicketController.php
@@ -6,6 +6,7 @@ namespace AppBundle\Controller;
 use AppBundle\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Ticket;
+use AppBundle\Entity\User;
 use AppBundle\Form\CommentType as FormCommentType;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -13,6 +14,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class TicketController extends Controller
 {
@@ -41,21 +45,63 @@ class TicketController extends Controller
      * @Route("/tickets/assign", name="assign_tickets")
      * @Method({"GET", "POST"})
      */
-    public function assignTickets(Request $request)
+    public function assignTickets(Request $request, EntityManagerInterface $em, SessionInterface $session)
     {
         // Check if user is an admin
         if (!$this->isGranted('ROLE_ADMIN')) {
             throw $this->createAccessDeniedException();
         }
 
-        // Handle the form submission logic
+        // Retrieve form data from POST request
         $selectedTickets = $request->request->get('selected_tickets', []);
-        $adminUsername = $request->request->get('admin_username');
+        $adminUsername = $request->request->get('admin_username', '');
 
-        // Process the selected tickets...
-        // Logic to assign the tickets
+        // Start the session to store flash messages
+        $session = new Session();
 
-        return $this->redirectToRoute('ticket_list');
+        if (!empty($selectedTickets) && !empty($adminUsername)) {
+            $em = $this->getDoctrine()->getManager();
+            $userRepository = $em->getRepository('AppBundle:User');
+
+            // Fetch the admin user entity by username
+            $adminUser = $userRepository->findOneBy(['username' => $adminUsername]);
+
+            if ($adminUser) {
+                foreach ($selectedTickets as $ticketId) {
+                    // Assign ticket to admin user
+                    $ticketUserStmt = $em->getConnection()->prepare(
+                        "INSERT INTO ticket_user (ticket_id, user_id) 
+                        SELECT :ticket_id, u.id 
+                        FROM users u 
+                        WHERE u.username = :admin_username"
+                    );
+                    $ticketUserStmt->execute(['ticket_id' => $ticketId, 'admin_username' => $adminUsername]);
+
+                    // Update the ticket status
+                    $ticketStmt = $em->getConnection()->prepare(
+                        "UPDATE tickets 
+                        SET status = 'In Progress', assigned_to = :admin_username, reviewed_by = :admin_username 
+                        WHERE id = :ticket_id"
+                    );
+                    $ticketStmt->execute([
+                        'admin_username' => $adminUsername,
+                        'ticket_id' => $ticketId
+                    ]);
+                }
+
+                // Flash success message
+                $session->getFlashBag()->add('success', "Tickets successfully assigned to $adminUsername!");
+            } else {
+                // Flash error message if admin user is not found
+                $session->getFlashBag()->add('error', "Admin user not found.");
+            }
+        } else {
+            // Flash error message if no tickets or admin username is provided
+            $session->getFlashBag()->add('error', "No tickets selected or admin username is missing.");
+        }
+
+        // Redirect back to the ticket list page or some other page
+        return $this->redirectToRoute('homepage'); 
     }
 
     /**
@@ -76,14 +122,20 @@ class TicketController extends Controller
         // Handle image upload
         $imagePath = '';
         $file = $request->files->get('image');
+
         if ($file instanceof UploadedFile) {
+        // die(dump($file));
+        // die(dump($this->getParameter('uploads_directory')));
+
             try {
                 $fileName = md5(uniqid()) . '.' . $file->guessExtension();
                 $file->move($this->getParameter('uploads_directory'), $fileName);
+                // $file->move($this->getParameter('kernel.project_dir').'/web/uploads', $fileName);
                 $imagePath = $fileName;
             } catch (FileException $e) {
                 // Handle the error (e.g., log it or set an error message)
                 $this->addFlash('error', 'Failed to upload image.');
+                die(dump($e));
                 return $this->redirectToRoute('ticket_create');
             }
         }
@@ -105,7 +157,7 @@ class TicketController extends Controller
         $ticket->setCreatedBy($createdBy);
         $ticket->setReviewedBy($reviewedBy);
         $ticket->setStatus($status);
-        $ticket->setImage($imagePath);
+        $ticket->setImage("uploads/".$imagePath);
         $ticket->setCreatedAt(new \DateTime());
         $ticket->setUpdatedAt(new \DateTime());
 
@@ -115,19 +167,17 @@ class TicketController extends Controller
 
         // Redirect back to the form with a success message
         $this->addFlash('success', 'Ticket created successfully!');
-        return $this->redirect($request->headers->get('referer'));
+        return $this->redirectToRoute('homepage');
     }
 
     /**
-     * @Route("/ticket/{id}", name="ticket_detail")
+     * @Route("/ticket/{id}/detail", name="ticket_detail")
      */
     public function ticketDetailAction($id, Request $request)
     {
         // // Create the comment form
         // $commentForm = $this->createForm(FormCommentType::class);
         // $commentForm->handleRequest($request);
-
-        $previousUrl = $this->get('request_stack')->getMasterRequest()->headers->get('referer');
 
         $em = $this->getDoctrine()->getManager();
 
@@ -136,9 +186,13 @@ class TicketController extends Controller
 
         // die(dump($ticket));
 
-        if (!$ticket) {
-            throw $this->createNotFoundException('Ticket not found.');
-        }
+        // if (!$ticket) {
+        //     throw $this->createNotFoundException('Ticket not found.');
+        // }
+
+        // Handle image upload
+        
+
 
         $isowner = false;
         if ($this->getUser() && $this->getUser()->getUsername() == $ticket->getCreatedBy()) {
@@ -155,7 +209,6 @@ class TicketController extends Controller
             'ticket' => $ticket,
             'comments' => $comments,
             // 'commentForm' => $commentForm->createView(),
-            'previousUrl' => $previousUrl,
             'isowner' => $isowner,
         ]);
     }
@@ -188,12 +241,33 @@ class TicketController extends Controller
             // throw $this->createNotFoundException('Ticket not found');
         }
 
+        $imagePath = $ticket->getImage();
+        $file = $request->files->get('image');
+
+        if ($file instanceof UploadedFile) {
+        // die(dump($file));
+        // die(dump($this->getParameter('uploads_directory')));
+
+            try {
+                $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+                $file->move($this->getParameter('uploads_directory'), $fileName);
+                // $file->move($this->getParameter('kernel.project_dir').'/web/uploads', $fileName);
+                $imagePath = "uploads/".$fileName;
+            } catch (FileException $e) {
+                // Handle the error (e.g., log it or set an error message)
+                $this->addFlash('error', 'Failed to upload image.');
+                die(dump($e));
+                return $this->redirectToRoute('ticket_create');
+            }
+        }
+
         // Update the ticket entity
         $ticket->setDepartment($department);
         $ticket->setType($type);
         $ticket->setPriority($priority);
         $ticket->setTitle($title);
         $ticket->setContent($content);
+        $ticket->setImage($imagePath);
 
         // Persist changes to the database
         $em->flush();
@@ -263,5 +337,93 @@ class TicketController extends Controller
         // Redirect back to the previous page
         $this->addFlash('success', 'Comment added successfully.');
         return $this->redirect($request->headers->get('referer'));
+    }
+
+    /**
+     * @Route("/ticket/filter", name="filter_ticket", methods={"GET"})
+     */
+    public function filterTicketAction(Request $request, EntityManagerInterface $em)
+    {
+        $searchText = $request->query->get('search', '');
+        $selectedDepartment = strtolower($request->query->get('department', 'all'));
+        $selectedPriority = strtolower($request->query->get('priority', 'all'));
+        $selectedStatus = strtolower($request->query->get('status', 'all'));
+        $selectedType = strtolower($request->query->get('type', 'all'));
+        $selectedResponsible = strtolower($request->query->get('responsible', 'all'));
+        $selectedWeek = strtolower($request->query->get('week', 'all'));
+
+        $qb = $em->createQueryBuilder();
+        $qb->select('t')
+            ->from('AppBundle:Ticket', 't')
+            ->where('1 = 1');
+
+        // Apply search filters
+        if (!empty($searchText)) {
+            $qb->andWhere('t.title LIKE :searchText')
+               ->setParameter('searchText', '%' . $searchText . '%');
+        }
+
+        if ($selectedDepartment !== 'all') {
+            $qb->andWhere('LOWER(t.department) = :department')
+               ->setParameter('department', $selectedDepartment);
+        }
+
+        if ($selectedPriority !== 'all') {
+            $qb->andWhere('LOWER(t.priority) = :priority')
+               ->setParameter('priority', $selectedPriority);
+        }
+
+        if ($selectedStatus !== 'all') {
+            $qb->andWhere('LOWER(t.status) = :status')
+               ->setParameter('status', $selectedStatus);
+        }
+
+        if ($selectedType !== 'all') {
+            $qb->andWhere('LOWER(t.type) = :type')
+               ->setParameter('type', $selectedType);
+        }
+
+        if ($selectedResponsible !== 'all') {
+            $qb->andWhere('LOWER(t.assignedTo) = :responsible')
+               ->setParameter('responsible', $selectedResponsible);
+        }
+
+        if ($selectedWeek !== 'all') {
+            switch ($selectedWeek) {
+                case 'week-1':
+                    $startOfWeek = (new \DateTime())->modify('first day of this month')->format('Y-m-d');
+                    $endOfWeek = (new \DateTime($startOfWeek))->modify('+6 days')->format('Y-m-d');
+                    break;
+                case 'week-2':
+                    $startOfWeek = (new \DateTime())->modify('first day of this month')->modify('+7 days')->format('Y-m-d');
+                    $endOfWeek = (new \DateTime($startOfWeek))->modify('+6 days')->format('Y-m-d');
+                    break;
+                case 'week-3':
+                    $startOfWeek = (new \DateTime())->modify('first day of this month')->modify('+14 days')->format('Y-m-d');
+                    $endOfWeek = (new \DateTime($startOfWeek))->modify('+6 days')->format('Y-m-d');
+                    break;
+                case 'week-4':
+                    $startOfWeek = (new \DateTime())->modify('first day of this month')->modify('+21 days')->format('Y-m-d');
+                    $endOfWeek = (new \DateTime())->format('Y-m-t'); // Last day of the month
+                    break;
+            }
+            $qb->andWhere('t.createdAt BETWEEN :startOfWeek AND :endOfWeek')
+               ->setParameter('startOfWeek', $startOfWeek)
+               ->setParameter('endOfWeek', $endOfWeek);
+        }
+
+        $qb->orderBy('t.createdAt', 'DESC');
+        $tickets = $qb->getQuery()->getResult();
+
+        if (count($tickets) > 0) {
+            // Render the ticket partials
+            $responseContent = $this->renderView('ticket/_ticket_list.html.twig', [
+                'tickets' => $tickets,
+            ]);
+        } else {
+            $responseContent = "<p>No tickets found.</p>";
+        }
+
+        return new Response($responseContent);
     }
 }
